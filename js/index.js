ff30d01d1430c1877592866cb3a3006c7941a996
eric-m-rodriguez <ericm.rodriguez@gmail.com>
2025-02-09 16:45:10 -0800

    updated notation to better reflect project intent

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 /* Fetch Open-Meteo API */
-let githubUsername = 'eric-m-rodriguez'; /*creates variable for GitHub username*/
-
 let weatherUrl = 'https://api.open-meteo.com/v1/forecast?latitude=45.5234&longitude=-122.6762&current=temperature_2m,is_day,precipitation,cloud_cover,wind_speed_10m,wind_gusts_10m&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch'; /*creates variable for GitHub URL*/
 
-fetch (weatherUrl) /*fetches data from GitHub API*/
+fetch (weatherUrl) /*fetches data from Open-Meteo API*/
     .then(response => response.json()) /*parses response as JSON*/
-    .then(repositories => {
-        console.log(repositories); /*logs repos to console*/  
+    .then(data => {
+        console.log(data); /*logs weather data to console*/  
     })
     
     .catch (error => 
-        console.error("Error", error.message)); /*logs error message to the console */
+        console.error("Error: API", error.message)); /*logs error message to the console */
 
 /*Adding updates here to create new lesson-13 pull request; to delete later*/
